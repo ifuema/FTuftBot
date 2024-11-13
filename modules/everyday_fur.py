00c05fe4ea3b802be96c2bf804810da9c20f3a8c
ifuema <y3bpcy3bpc@gmail.com>
2024-11-13 11:07:43 +0800

    每日一毛

---
+++ modules/everyday_fur.py
@@ -0,0 +1,30 @@
+from graia.amnesia.builtins import aiohttp
+from graia.ariadne import Ariadne
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Image
+from graia.scheduler import timers
+from graia.scheduler.saya import SchedulerSchema
+from graia.saya import Channel
+
+import bot_config
+
+channel = Channel.current()
+shouyunji_random = "https://cloud.foxtail.cn/api/function/random?type=1"
+shouyunji_pictures = "https://cloud.foxtail.cn/api/function/pictures?model=1&"
+
+
+@channel.use(SchedulerSchema(timers.crontabify(bot_config.EVERYDAY_FUR_TIME)))
+async def everyday_fur(app: Ariadne):
+    for group in bot_config.EVERYDAY_FUR_GROUPS:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(shouyunji_random) as response:
+                if response.status != 200:
+                    print(f"请求失败，状态码：{response.status}")
+                    return
+                p_id = (await response.json())['picture']['id']
+            async with session.get(shouyunji_pictures + "picture=" + p_id) as response:
+                if response.status != 200:
+                    print(f"请求失败，状态码：{response.status}")
+                    return
+                res = await response.json()
+            await app.send_group_message(group, MessageChain("【每日一毛】" + res['name']) + MessageChain(Image(url=res['url'])))
